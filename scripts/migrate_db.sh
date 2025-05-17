c577b92771f5f0c86013a72506f020d3f24434b1
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-05-17 16:28:02 +0800

    fix: update database migration script to retrieve the latest instance's private IP and enhance SSH command with strict host key checking disabled

--- scripts/migrate_db.sh
+++ scripts/migrate_db.sh
@@ -16,7 +16,7 @@ PRIVATE_IP=$(
     aws ec2 describe-instances \
         --region ap-southeast-1 \
         --filters "Name=tag:Role,Values=application" "Name=instance-state-name,Values=running" \
-        --query "Reservations[0].Instances[0].PrivateIpAddress" \
+        --query "sort_by(Reservations[*].Instances[*][], &LaunchTime)[-1].PrivateIpAddress" \
         --output text
 )
 
@@ -24,5 +24,7 @@ KEY_PATH=~/.ssh/private_key.pem
 
 echo "Migrating database on $PRIVATE_IP via $BASTION_HOST_IP"
 
-ssh -i $KEY_PATH -o ProxyCommand="ssh -i $KEY_PATH -W %h:%p $USER@$BASTION_HOST_IP" $USER@$PRIVATE_IP \
+ssh -i $KEY_PATH -o StrictHostKeyChecking=no -o ProxyCommand="ssh -i $KEY_PATH -o StrictHostKeyChecking=no -W %h:%p $USER@$BASTION_HOST_IP" $USER@$PRIVATE_IP \
   "cd fastapi-app && docker-compose --env-file .env -f docker-compose.prod.yml exec fastapi alembic upgrade head && echo Done Migration"
+
+echo "Done Migration"
